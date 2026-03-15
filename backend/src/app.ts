a9837a051947f9dbded56868a97c02022ee19337
Jorgend <Fullsteck@yandex.kz>
2026-03-15 08:17:38 +0500

    test 0.0.1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,19 +9,22 @@ import errorHandler from './middlewares/error-handler';
 import { requestLogger, errorLogger } from './middlewares/logger';
 
 const { PORT = 3000 } = process.env;
+const { DB_ADDRESS = "mongodb://127.0.0.1:27017/weblarek" } = process.env
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/weblarek');
+mongoose.connect(DB_ADDRESS);
 app.use(express.static(path.join(__dirname, './public')));
 
 app.use(cors());
+app.use(requestLogger)
 
 app.use('/product', productRoutes);
 app.use('/order', orderRoutes);
 
 app.use(errors());
+app.use(errorLogger)
 app.use(errorHandler);
 
 app.listen(PORT, () => {
